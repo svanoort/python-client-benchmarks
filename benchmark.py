41857f77008b1214f7c03e883400ee1507be60dc
Sam Van Oort <samvanoort@gmail.com>
2016-02-23 21:26:42 -0500

    Add a delay option to avoid overwhelming servers

--- benchmark.py
+++ benchmark.py
@@ -15,21 +15,22 @@ except:
         from io import StringIO
 import requests, urllib, urllib2, urllib3
 
-def run_test(library, url, cycles, connection_reuse, options, setup_test, run_test):
+def run_test(library, url, cycles, connection_reuse, options, setup_test, run_test, delay=0.05):
     """ Runs a benchmark, showing start & stop 
         the setup_test is a String.template with $url as an option
         the run_test allows for the same
     """
     print("START testing {0} performance with {1} cycles and connection reuse {2}".format(library, cycles, connection_reuse))
     print("Options: {0}".format(options))
-    mytime = timeit.timeit(stmt=string.Template(run_test).substitute(url=url),
+    mytime = timeit.timeit(stmt=string.Template(run_test).substitute(url=url)+"; time.sleep({0})".format(delay),
         setup=string.Template(setup_test).substitute(url=url), 
         number=cycles)
+    mytime = mytime - (delay * cycles)
     print("END testing result: {0}".format(mytime))
     result = (library, connection_reuse, options, cycles, mytime)
     return result
 
-def run_all_benchmarks(url='', cycles=10, **kwargs):
+def run_all_benchmarks(url='', cycles=10, delay=0.05, **kwargs):
     results = list()
 
     headers = ('Library','Reuse Connections?','Options', 'Time')
@@ -103,13 +104,14 @@ def run_all_benchmarks(url='', cycles=10, **kwargs):
         "body = urllib.urlopen('$url').read()"))
 
     for test in tests:
-        my_result = run_test(test[0], url, cycles, test[1], test[2], test[3], test[4])
+        my_result = run_test(test[0], url, cycles, test[1], test[2], test[3], test[4], delay=delay)
         results.append((test[0], test[1], test[2], my_result))
 
 if(__name__ == '__main__'):
     parser = argparse.ArgumentParser(description="Benchmark different python request frameworks")
     parser.add_argument('--url', metavar='u', type=str, default='http://localhost:5000/ping', help="URL to run requests against")
     parser.add_argument('--cycles', metavar='c', type=int, default=1000, help="Number of cycles to run")    
+    parser.add_argument('--delay', metavar='d', type=float, default=0.05, help="Delay in seconds between requests")    
     parser.add_argument('--output-file', metavar='o', type=str, help="Output file to write CSV results to")
     args = vars(parser.parse_args())
     if args.get('url') is None:
