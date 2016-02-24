7d04a149d792324131667743a241bcb1e02c0280
Sam Van Oort <samvanoort@gmail.com>
2016-02-24 08:28:31 -0500

    Rework delay parameter to be off by default, add whitespace after benchmark

--- benchmark.py
+++ benchmark.py
@@ -15,22 +15,31 @@ except:
         from io import StringIO
 import requests, urllib, urllib2, urllib3
 
-def run_test(library, url, cycles, connection_reuse, options, setup_test, run_test, delay=0.05):
+def run_test(library, url, cycles, connection_reuse, options, setup_test, run_test, delay=None):
     """ Runs a benchmark, showing start & stop 
         the setup_test is a String.template with $url as an option
         the run_test allows for the same
     """
     print("START testing {0} performance with {1} cycles and connection reuse {2}".format(library, cycles, connection_reuse))
     print("Options: {0}".format(options))
-    mytime = timeit.timeit(stmt=string.Template(run_test).substitute(url=url)+"; time.sleep({0})".format(delay),
-        setup=string.Template(setup_test).substitute(url=url), 
+
+    run_cmd = string.Template(run_test).substitute(url=url)
+    if delay:
+        run_cmd = run_cmd + "; time.sleep({0})".format(delay)
+    setup_cmd = string.Template(setup_test).substitute(url=url)
+
+    mytime = timeit.timeit(stmt=run_cmd,
+        setup=setup_cmd, 
         number=cycles)
-    mytime = mytime - (delay * cycles)
+    if delay:
+        mytime = mytime - (delay * cycles)
+
     print("END testing result: {0}".format(mytime))
+    print(' ')
     result = (library, connection_reuse, options, cycles, mytime)
     return result
 
-def run_all_benchmarks(url='', cycles=10, delay=0.05, **kwargs):
+def run_all_benchmarks(url='', cycles=10, delay=None, **kwargs):
     results = list()
 
     headers = ('Library','Reuse Connections?','Options', 'Time')
@@ -111,8 +120,8 @@ def run_all_benchmarks(url='', cycles=10, delay=0.05, **kwargs):
 if(__name__ == '__main__'):
     parser = argparse.ArgumentParser(description="Benchmark different python request frameworks")
     parser.add_argument('--url', metavar='u', type=str, default='http://localhost:5000/ping', help="URL to run requests against")
-    parser.add_argument('--cycles', metavar='c', type=int, default=1000, help="Number of cycles to run")    
-    parser.add_argument('--delay', metavar='d', type=float, default=0.05, help="Delay in seconds between requests")    
+    parser.add_argument('--cycles', metavar='c', type=int, default=10000, help="Number of cycles to run")    
+    parser.add_argument('--delay', metavar='d', type=float, help="Delay in seconds between requests")    
     parser.add_argument('--output-file', metavar='o', type=str, help="Output file to write CSV results to")
     args = vars(parser.parse_args())
     if args.get('url') is None:
