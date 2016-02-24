b56c2063dbb8ea6145048eb8a74bfd2693b2b6f4
Sam Van Oort <samvanoort@gmail.com>
2016-02-24 08:55:44 -0500

    Add bigger response payload option of 512B

--- app.py
+++ app.py
@@ -6,5 +6,31 @@ app = Flask(__name__)
 def hello():
     return "pong"
 
+# Returns larger sample JSON from http://json.org/example.html to exercise performance with larger payloads
+@app.route("/bigger")
+def big_response():
+	return '''{ 
+    "glossary": { 
+        "title": "example glossary", 
+		"GlossDiv": {
+            "title": "S",
+			"GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+					"SortAs": "SGML",
+					"GlossTerm": "Standard Generalized Markup Language",
+					"Acronym": "SGML",
+					"Abbrev": "ISO 8879:1986",
+					"GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+						"GlossSeeAlso": ["GML", "XML"]
+                    },
+					"GlossSee": "markup"
+                }
+            }
+        }
+    }
+}'''
+
 if __name__ == "__main__":
     app.run()
